c9b1cab530f2a6546d5c3c5b1a5fca1e3c0445be
Dev Bramm <bramwellokoth@gmail.com>
2021-12-12 23:06:13 +0300

    commit

---
+++ lib/screens/sign_up_options/sign_up_options_screen.dart
@@ -0,0 +1,11 @@
+import 'package:around_the_world/screens/sign_up/components/body.dart';
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
